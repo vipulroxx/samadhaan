94975c31431c13d8d53606ebcbc7b5550d655c07
vipulsharma <vipulsharma936@gmail.com>
2021-05-30 14:15:51 +0530

    Added style

--- concern.php
+++ concern.php
@@ -20,7 +20,7 @@
             <h1>Hello, <?php echo $_SESSION['name']; ?>!</h1>
             <a href="logout.php">Logout</a>
         </div>
-        <div class="concern-container" style="width:100%;">
+        <div class="concern-container" style="width:60%;">
             <?php echo "<h2>Concern of ".strtoupper($row['name'])."</h2>"?>
             <form class="attend-concern" action="attend.php"><button type="submit">ATTEND</button></form>
             <?php echo "<h4>CATEGORY: <u>".strtoupper($row['category'])."</u></h4>"?>
@@ -30,7 +30,7 @@
             <?php echo "<h4>AGENCY: <u>".strtoupper($row['agency'])."</u></h4>"?>
             <?php echo "<h4>PRICE: ".$row['price']." Rupees</h4>"?>
             <?php echo "<h4>CONCERN: ".$row['concern']."</h4>"?>
-            <a href="admin.php">BACK TO CONCERN LIST</a>
+            
             <figure>
                 <img src='<?php echo $row['image']; ?>' >
                 <figcaption><b>BEFORE</b></figcaption>
@@ -41,6 +41,7 @@
                 <figcaption><b>AFTER</b></figcaption>
             </figure><br>
             <?php } ?>
+            <a href="admin.php">BACK TO CONCERN LIST</a>
         </div>
         <?php } ?>
     </body>
